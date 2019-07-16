1a39720564bbdaeee54695b2221fc849e53c286a
Owersun <owersun@gmail.com>
2019-07-16 12:11:17 +0300

    Add Odroid-N1 board

---
+++ include/configs/odroidn1_rk3399.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * (C) Copyright 2016 Rockchip Electronics Co., Ltd
+ */
+
+#ifndef __ODROIDN1_RK3399_H
+#define __ODROIDN1_RK3399_H
+
+#include <configs/rk3399_common.h>
+
+#define CONFIG_SYS_MMC_ENV_DEV		1
+
+#define CONFIG_SYS_SATA_MAX_DEVICE	1
+
+#define SDRAM_BANK_SIZE			(2UL << 30)
+
+#endif
